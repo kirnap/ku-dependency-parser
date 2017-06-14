7a621417a1a17329179b48a6f79507bbb17dde33
omer kirnap <okirnap@ku.edu.tr>
2017-06-14 09:32:26 +0300

    create scratch data handler

--- test/set_testenv.jl
+++ test/set_testenv.jl
@@ -3,3 +3,7 @@ const fake_data = "../data/foo.conllu"
 const real_tdata = "/state/partition1/tmp/okirnap/UD_English/en-ud-train.conllu"
 const chmodel = "../data/english_chmodel.jld" # converted chmodelfile
 const real_ddata = "/state/partition1/tmp/okirnap/UD_English/en-ud-dev.conllu"
+
+const sc_tdata = "/ai/data/nlp/conll17/ud-treebanks-v2.0/UD_English/en-ud-train.conllu"
+const sc_ddata = "/ai/data/nlp/conll17/ud-treebanks-v2.0/UD_English/en-ud-dev.conllu"
+const sc_chmodel = "/Users/okirnap/charNL/trained_models/converged/english_chmodel.jld"
